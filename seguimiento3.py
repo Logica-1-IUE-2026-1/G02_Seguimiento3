d01176e921fae5b2882449df0e64aa1e48ac305a
aldanabuelvasireidis-hub <aldanabuelvasireidis@gmail.com>
2026-04-22 19:17:17 -0500

    conversion en peso

---
+++ seguimiento3.py
@@ -0,0 +1,14 @@
+    """Esta función calcula la conversión entre diferentes unidades de medida kilogramos a libras, gramos a onzas, kilogramos a toneladas"""
+    def kilogramos_libras(kg):
+        """
+        esta funcion calcula la conversión entre las unidades de medidas.
+        """
+        z = kg * 2.20462
+        return z
+    def Gramos_onzas(g):
+        z = g * 0.035274 
+        return z
+
+    def kilogramos_toneladas(kg):
+        z = kg / 1000
+        return z
